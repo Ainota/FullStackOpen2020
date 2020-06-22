eb13c420087aae24fbf9a2a9a9511fbd9ad40c4c
Aino Taipalus <aino.taipalus@siemens.com>
2020-06-22 09:44:34 +0300

    Exc1.5

--- exercises/1/kurssitiedot/src/index.js
+++ exercises/1/kurssitiedot/src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
 const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
     </div>
-  );
-};
+  )
+}
 
 const Part = (props) => {
   return (
@@ -16,8 +16,8 @@ const Part = (props) => {
         {props.partName} {props.partAmountExc}
       </p>
     </>
-  );
-};
+  )
+}
 
 const Content = (props) => {
   return (
@@ -35,8 +35,8 @@ const Content = (props) => {
         partAmountExc={props.parts[2].exercises}
       />
     </div>
-  );
-};
+  )
+}
 
 const Total = (props) => {
   return (
@@ -47,33 +47,35 @@ const Total = (props) => {
           props.parts[2].exercises}
       </p>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
