7dac71bbb7b954732f8665a633056af100826e6f
Aino Taipalus <aino.taipalus@siemens.com>
2020-06-22 09:20:43 +0300

    Exercise1.3

---
+++ course/part1/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+
+  return (
+    <>
+    <div>
+      <h1>Hello</h1>
+      <Hello name = "Daniel" age={5 + 10} />
+      <Hello name = {nimi} age = {ika} />
+    </div>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
