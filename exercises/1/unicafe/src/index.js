71a5ad923d3e97930a09f3cd9eed1efe7cb0863b
Aino Taipalus <aino.taipalus@siemens.com>
2020-06-26 12:20:28 +0300

    Exc.1.7

--- exercises/1/unicafe/src/index.js
+++ exercises/1/unicafe/src/index.js
@@ -17,6 +17,12 @@ const App = () => {
         good {good} <br />
         neutral {neutral} <br />
         bad {bad}
+        <br />
+        all {good + neutral + bad}
+        <br />
+        average {(good - bad) / (good + neutral + bad)}
+        <br />
+        positive {(good / (good + neutral + bad))*100} %<br />
       </div>
     </div>
   )
